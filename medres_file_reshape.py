695626c4b334919cfcc1997c5c9cc37858548215
Aidan McBride <aidan.mcbride@utah.edu>
2022-05-25 01:21:52 -0600

    Big updates - data structure done, CA residuals starting. transfer data local temp

--- medres_file_reshape.py
+++ medres_file_reshape.py
@@ -1,44 +1,120 @@
 from astropy.io import fits
+from astropy.table import Table
 import numpy as np
+import numpy.ma as ma
 import os
 import tqdm
 import glob
 
 current_path = 'ResidualsFiles/'
-new_path = 'ResidualsFiles/Test/'
+new_path = 'Residuals/'
 meta_path = new_path + 'meta.fits'
 
-# teff_bins = np.concatenate([np.arange(3000, 5000, 50), np.arange(5000,10000, 150), [10000]])
-# logg_bins = np.around(np.arange(-1, 5.2, 0.2), 2)
-# m_h_bins = np.around(np.concatenate([np.arange(-2.3, -1.1, 0.3), np.arange(-1.1, 0.5, 0.2), [0.5]]), 2)
+cols = ['TEFF_MIN', 'TEFF_MAX', 'LOGG_MIN', 'LOGG_MAX', 'M_H_MIN', 'M_H_MAX', 
+       'NBIN', 'STDEV_MEDIAN', 'STDEV_MEAN', 'FNAME']
+
+size = 26640 # number of individual files
+table_contents = np.zeros((size, len(cols)), dtype = 'object')
+counter = 0
+
+def updateMetaTable(index, items, contents = table_contents):
+    contents[index, :] = items
+    return contents
 
 fnames = glob.glob(current_path + '*')
+fnames.remove(current_path + 'meta_table.fits')
 
-for name in tqdm(fnames, total = len(fnames)):
+data_names = ['MEDIAN', 'MEAN', 'STDEV', 'MED_ABS_DEV', 'PERC16', 'PERC84', 'NPIX']
+
+for name in tqdm.tqdm(fnames, total = len(fnames)):
     hdul = fits.open(name)
     header = hdul[1].header
     teff_min, teff_max = (header['TMIN'], header['TMAX'])
+    CRVAL1 = header['CRVAL1']
+    CDELT1 = header['CDELT1']
     
-    teff_dir = new_path + 'TEFF_{min}_{max}/'.format(min = str(teff_min), max = str(teff_max))
-    os.mkdir(teff_dir)
+#     teff_dir = new_path + 'TEFF_{min}_{max}/'.format(min = str(teff_min), max = str(teff_max))
+    
+#     try:
+#         os.mkdir(teff_dir)
+#     except OSError as error:
+#         pass
     
     hdul = np.array(hdul)[1:].reshape(30,12)
     
     for hdul_bin in hdul:
         header = hdul_bin[0].header
         logg_min, logg_max = (header['GMIN'], header['GMAX'])
-        logg_dir = teff_dir + 'LOGG_{min}_{max}/'.format(min = str(logg_min), max = str(teff_max))
+#         logg_dir = teff_dir + 'LOGG_{min}_{max}/'.format(min = str(logg_min), max = str(logg_max))
+        
+#         try:
+#             os.mkdir(logg_dir)
+#         except OSError as error:
+#             pass
         
         for hdu in hdul_bin:
+            m_h_min, m_h_max = (header['MMIN'], header['MMAX'])
+            nstars = header['NBIN']
             
-            
-        
-        
-#     hdu = hdulist[j, k].item()
-                                                  
-    
-    break
+            hdu0 = fits.PrimaryHDU()
+            hdu0.header['NBIN'] = nstars
+            hdu0.header['TEFF_MIN'] = teff_min
+            hdu0.header['TEFF_MAX'] = teff_max
+            hdu0.header['LOGG_MIN'] = logg_min
+            hdu0.header['LOGG_MAX'] = logg_max
+            hdu0.header['M_H_MIN'] = m_h_min
+            hdu0.header['M_H_MAX'] = m_h_max
+            hdu0.header['CRVAL1'] = CRVAL1
+            hdu0.header['CDELT1'] = CDELT1
 
+            hdulist = fits.HDUList([hdu0])
+            
+            for i in range(1, 7):
+                if hdu.data is not None:
+                    h = fits.ImageHDU(hdu.data[i])
+                else: 
+                    h = fits.ImageHDU()
+                    
+                h.header['NAME'] = data_names[i-1]
+                h.header['NBIN'] = nstars
+                h.header['TEFF_MIN'] = teff_min
+                h.header['TEFF_MAX'] = teff_max
+                h.header['LOGG_MIN'] = logg_min
+                h.header['LOGG_MAX'] = logg_max
+                h.header['M_H_MIN'] = m_h_min
+                h.header['M_H_MAX'] = m_h_max
+                h.header['CRVAL1'] = CRVAL1
+                h.header['CDELT1'] = CDELT1
+   
+                hdulist.append(h)
+            if hdu.data is None:
+                stdev_median = np.nan
+                stdev_mean = np.nan
+            else:
+                median = ma.array(hdulist[1].data, mask = hdulist[1].data == 0.0)
+                mean = ma.array(hdulist[2].data, mask = hdulist[2].data == 0.0)
+                stdev_median = ma.std(median, ddof = 1)
+                stdev_mean = ma.std(median, ddof = 1)
+            
+            
+            res_name = 'TEFFBIN{tmin}_{tmax}_LOGGBIN{gmin}_{gmax}_M_HBIN{mmin}_{mmax}.fits'.format(
+                                                                tmin = teff_min, tmax= teff_max, 
+                                                                gmin = logg_min, gmax = logg_max, 
+                                                                mmin = m_h_min, mmax = m_h_max)
+            fname_res = new_path + res_name
+            hdulist.writeto(fname_res, overwrite = True)
+            
+            row_update = [teff_min, teff_max, logg_min, logg_max, m_h_min, m_h_max, nstars, stdev_median, stdev_mean, res_name]
+            table_contents[counter,:] = row_update
+            counter = counter + 1
+
+datatype = [int, int, float, float, float, float, int, float, float, str]
+meta_table = Table(names = cols, data = table_contents)
+# tabcols = meta_table.columns
+for i in range(len(cols)):
+    col = cols[i]
+    meta_table[col] = meta_table[col].astype(datatype[i])
+meta_table.write(meta_path, format = 'fits', overwrite = True)
 
 
 ##### DATA STRUCTURE ######
